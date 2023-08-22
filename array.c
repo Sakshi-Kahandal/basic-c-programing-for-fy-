05db911d990f984986d3cb6d7739bc52933d6147
Sakshi-Kahandal <140343186+Sakshi-Kahandal@users.noreply.github.com>
2023-08-22 21:05:03 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int i,n,a[40];
+   printf("enter limit::");
+   scanf("%d",&n);
+   printf("enter n numbers::");
+   for(i=0;i<n;i++)
+   scanf("%d",&a[i]);
+   
+   printf("reverse numbers=");
+   for(i=n-1;i>=0;i--)
+   printf("%d\t",a[i]);
+}
