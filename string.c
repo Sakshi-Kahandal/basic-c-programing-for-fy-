34c484ddc1cf735eb537221c5244b62b68ef7479
Sakshi-Kahandal <140343186+Sakshi-Kahandal@users.noreply.github.com>
2023-08-22 21:02:28 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char s1[50];
+  int i;
+  printf("enter string::");
+  gets(s1);
+  
+  for(i=0;s1[i]!='\0';i++);
+  
+  printf("first character=%c",s1[0]);
+  printf("\nlast character=%c",s1[i-1]);
+}
